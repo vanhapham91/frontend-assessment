a9abdd9dd927cc453165c6fdf64c8925d0cde2bd
Hannah <vanhapham91@gmail.com>
2021-06-04 13:37:36 +1000

    Create Tabs component

---
+++ src/components/Tabs.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="tab-group">
+
+	</div>
+</template>
+
+<script>
+export default {
+name: "Tabs"
+}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,12 +4,19 @@
 			<img src="http://via.placeholder.com/1920x650"/>
 		</div>
 		<div>
+			<Tabs/>
 		</div>
 	</div>
 </template>
 
 <script>
+import Tabs from '../components/Tabs';
+
+
 export default {
   name: 'HelloWorld',
+	props: [
+    Tabs
+	]
 }
 </script>
