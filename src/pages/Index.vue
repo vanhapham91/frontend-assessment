a5fdb2ad5aa333c65636426fcd631e8f4d4c5a20
Hannah <vanhapham91@gmail.com>
2021-06-04 13:37:07 +1000

    Create Index page

---
+++ src/pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="indexPage">
+		<div class="index__banner">
+			<img src="http://via.placeholder.com/1920x650"/>
+		</div>
+		<div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
